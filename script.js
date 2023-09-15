58054c43e2b7f0136e33d560bc2e3c08af309b2e
gza-team <145040990+gza-team@users.noreply.github.com>
2023-09-15 17:11:21 +0700

    Create script.js for gza-team.github.io Github Pages

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', function() {
+  // Lấy nội dung từ API hoặc bất kỳ nguồn dữ liệu nào khác
+  // và hiển thị nó lên trang web
+  var contentElement = document.getElementById('content');
+  contentElement.innerHTML = '<img src="example_image.jpg" alt="Rule34 Image">';
+});
